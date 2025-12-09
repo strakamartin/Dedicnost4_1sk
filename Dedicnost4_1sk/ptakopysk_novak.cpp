2b2e29078971a9e01c185aea6a6d2ecafdbe5043
Ondřej Novák <novak.ondrej@sosasou.cz>
2025-12-09 09:55:24 +0100

    vytvoreni ptakopyska

---
+++ Dedicnost4_1sk/ptakopysk_novak.cpp
@@ -0,0 +1,8 @@
+//
+// Created by novak.ondrej on 09.12.2025.
+//
+
+#include "ptakopysk_novak.h"
+
+namespace std {
+} // std
---
+++ Dedicnost4_1sk/ptakopysk_novak.h
@@ -0,0 +1,13 @@
+//
+// Created by novak.ondrej on 09.12.2025.
+//
+
+#ifndef DEDICNOST4_1SK_SLN_PTAKOPYSK_NOVAK_H
+#define DEDICNOST4_1SK_SLN_PTAKOPYSK_NOVAK_H
+
+namespace std {
+    class ptakopysk_novak {
+    };
+} // std
+
+#endif //DEDICNOST4_1SK_SLN_PTAKOPYSK_NOVAK_H
